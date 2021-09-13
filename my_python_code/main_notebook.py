3fec43c1331df4827e929d864e58f0c96ad48b1a
Dustin VanStee <dvanstee@nvidia.com>
2021-09-13 09:14:47 -0400

    update from main.ipynb

--- my_python_code/main_notebook.py
+++ my_python_code/main_notebook.py
@@ -3,4 +3,19 @@
 __all__ = []
 
 # Cell
+# Setup in notebook flag.  Useful when you export code and have some situations where you dont want certain part of code run like visulaizations..
+import sys
+try: from nbdev.imports import IN_NOTEBOOK
+except: IN_NOTEBOOK=False
+
+if IN_NOTEBOOK :
+    print("In Notebooke mode")
+else :
+    print("Running in batch mode")
+
+# Cell
 import my_python_code.library_notebook as funcs
+
+
+# Cell
+print(f"IN_NOTEBOOK = {IN_NOTEBOOK}")
